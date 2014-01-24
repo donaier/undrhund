cac913fd0611558a0956efbeff200a4c71501132
Donat Baier <donat.baier@gmail.com>
2014-01-24 01:41:50 +0100

    use local path for local stuff

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,7 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+if Rails.env.development?
+  Paperclip::Attachment.default_options[:url] = '/public/uploads'
+else
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+end
+
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
