96d94830c77098afc4f443bb7694a1de45039bf1
Donat Baier <donat.baier@gmail.com>
2014-03-15 12:09:26 +0100

    tinymce new topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,10 +2,23 @@ class TopicsController < ApplicationController
 
   def index
     @topics = Topic.all
+    @topic = Topic.new(clan_member_id: current_clan_member.id)
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.update_attributes(visibility: 'visible')
+    redirect_to topics_path
   end
 
   def show
     @topic = Topic.find(params[:id])
     @post = Post.new(topic_id: params[:id], clan_member_id: current_clan_member.id)
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:category, :clan_member_id, :title, :text, :position, :visibility)
   end
 end
